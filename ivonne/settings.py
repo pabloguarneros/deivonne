615da6f23bf1653b9ec0cc87b4a01bd0dba4c30e
pabloguarneros <wrno.pablo@gmail.com>
2021-06-26 16:50:14 -0500

    Added Settings to Enable Heroku

--- ivonne/settings.py
+++ ivonne/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from decouple import config
 import os
+import django_heroku
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -98,3 +100,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
