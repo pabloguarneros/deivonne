f2052e6c1f090489625cb62537b89280097906d4
pabloguarneros <wrno.pablo@gmail.com>
2021-06-26 21:59:27 -0500

    Created models according to SDG Peaceful Institutions

--- contracts/models.py
+++ contracts/models.py
@@ -8,28 +8,50 @@ def contract_image_directory_path(instance, filename):
 
 class SmartContract(models.Model): 
 
+    '''
+    IMPORTANT!! Hash should be changed to models.UUIDField to prevent duplicate profiles for one specific contract!
+    However, for testing purposes (given association with SDGs proxies), we allow duplicates.
+    '''
+
     def __str__(self):
-        return self.poster.path.rsplit('/', 1)[-1]
-    
+        return self.title
+
     title = models.CharField(max_length=140, blank=True)
-    poster = models.ImageField(upload_to=contract_image_directory_path)
-    short_description = models.CharField(max_length=140, blank=True)
-    public_key = models.CharField(max_length=140, blank=True)
+    short_description = models.TextField()
+    hash = models.CharField(max_length=140, blank=True, default="KT1Puc9St8wdNoGtLiD2WXaHbWU7styaxYhD")
+
+    poster = models.ImageField(upload_to=contract_image_directory_path,blank=True)
+
 
     class SectorArea(models.IntegerChoices):
-        EDUCATION = 0
-        SUSTAINABILITY = 1
-        HEALTHSERVICES = 2
+        NOPOVERTY = 0
+        ZEROHUNGER = 1
+        GOODHEALTH = 2
+        QUALITYEDUCATION = 3
+        GENDEREQUALITY = 4
+        CLEANWATER = 5
+        AFFORDABLEENERGY = 6
+        DECENTWORK = 7
+        INDUSTRYINNOVATION = 8
+        REDUCEINEQUALITY = 9
+        SUSTAINABLECITIES = 10
+        RESPONSIBLECONSUMPTION = 11
+        CLIMATEACTION = 12
+        BELOWWATER = 13
+        ONLAND = 14
+        PEACEFULINSTITUIONS = 15
+        PARTNERSHIP = 16
 
-    sector = models.IntegerField(choices=SectorArea.choices, default=None, null=True)
+    sector = models.IntegerField(choices=SectorArea.choices, default=15)
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        img = Image.open(self.poster.path)
+        if self.poster != "":
+            img = Image.open(self.poster.path)
 
-        if img.height > 450 or img.width > 450:
-            new_img = (450, 450)
-            img.thumbnail(new_img)
-            img = img.convert('RGB') #convert transparency to new image!
-            img.save(self.poster.path)  # saving image at the same path
+            if img.height > 450 or img.width > 450:
+                new_img = (450, 450)
+                img.thumbnail(new_img)
+                img = img.convert('RGB') #convert transparency to new image!
+                img.save(self.poster.path)  # saving image at the same path
--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from contracts.models import SmartContract
 
 def home(request):
-    return render(request,"home/home.html")
+    context = {
+        "contracts":SmartContract.objects.all()
+    }
+    return render(request,"home/home.html",context)
